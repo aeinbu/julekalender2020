d119537b6aaffdef6cc0b30409a7105ee044f73f
Arjan Einbu <arjan@einbu.no>
2020-12-01 13:46:33 +0100

    Luke 1

---
+++ luke1/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open FSharpx
+
+[<EntryPoint>]
+let main argv =
+    let sumOfFile = "./data.txt" 
+                    |> File.ReadAllText
+                    |> String.splitChar [|','|]
+                    |> Array.sumBy Int64.Parse
+    
+    let sumOfSeq = seq {0L..100000L}
+                   |> Seq.sum
+    
+    let missingNumber = sumOfSeq-sumOfFile
+    
+    printf "I think that %d is missing" missingNumber
+    
+    0
